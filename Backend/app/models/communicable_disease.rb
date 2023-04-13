feab31b0dafba03da1f42adbcc3849c9d68447b3
shipwoli <khalwalesaviour@gmail.com>
2023-04-13 10:59:24 +0300

    reset the db

--- Backend/app/models/communicable_disease.rb
+++
@@ -1,5 +0,0 @@
-class CommunicableDisease < ApplicationRecord
-    has_many :areas
-    validates :name, presence: true, length: { maximum: 50 }
-    validates :description, :symptoms, :prevention_measures, presence: true
-end
--- Backend/app/models/donation.rb
+++
@@ -1,6 +0,0 @@
-class Donation < ApplicationRecord
-    belongs_to :user
-    belongs_to :area
-    validates :amount, presence: true, numericality: { greater_than: 0 }
-    validates :user_id, :area_id, presence: true
-end
