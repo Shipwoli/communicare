2bb98f0dd4c78a598cae150d621737997df4f4b8
AlexIrungu <aleaxmuiruri@gmail.com>
2023-04-17 11:36:13 +0300

    started migrations

--- Backend/db/migrate/20230417075923_create_reviews.rb
+++ Backend/db/migrate/20230417075923_create_reviews.rb
@@ -3,8 +3,8 @@ class CreateReviews < ActiveRecord::Migration[7.0]
     create_table :reviews do |t|
       t.text :content
       t.integer :rating
-      t.references :user_id, null: false, foreign_key: true
-      t.references :area_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :area, null: false, foreign_key: true
 
       t.timestamps
     end
--- Backend/db/migrate/20230417075954_create_donations.rb
+++ Backend/db/migrate/20230417075954_create_donations.rb
@@ -2,8 +2,8 @@ class CreateDonations < ActiveRecord::Migration[7.0]
   def change
     create_table :donations do |t|
       t.decimal :amount
-      t.references :user_id, null: false, foreign_key: true
-      t.references :area_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :area, null: false, foreign_key: true
 
       t.timestamps
     end
