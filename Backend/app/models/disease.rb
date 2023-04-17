18093717ebe93a4399cbb69474a66c43dfa7ce72
shipwoli <khalwalesaviour@gmail.com>
2023-04-17 10:01:01 +0300

    setting up postgres

--- Backend/app/models/disease.rb
+++
@@ -1,6 +0,0 @@
-class Disease < ApplicationRecord
-    has_many :areas
-
-    validates :name, presence: true, length: { maximum: 50 }
-    validates :description, :symptoms, :prevention, presence: true
-end
