7a5ec8f17837857dc4195e38609af47f11a6b483
Marylucy123450 <117809129+Marylucy123450@users.noreply.github.com>
2023-04-19 10:38:38 +0300

    delited donation

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import About from './components/About';
-import Donation from './components/Donation';
 export default function App() {
   return (
     <div className="App">
@@ -9,7 +8,7 @@ export default function App() {
 {/* added routes */}
       <Routes>
         <Route path="/" element={< About />} />
-        <Route path="/donation" element={< Donation />} />
+        
         
       </Routes>
       </BrowserRouter>
