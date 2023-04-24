6c0f10943e7de32db11de49d006135cdb008c0f7
shipwoli <khalwalesaviour@gmail.com>
2023-04-24 10:24:53 +0300

    updated models

--- Backend/app/controllers/users_controller.rb
+++ Backend/app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
     private 
     
     def user_params
-        params.permit(:first_name, :last_name, :email, :password, :admin)
+        params.permit(:username, :email, :password, :admin)
     end
 
 end
--- Backend/db/schema.rb
+++ Backend/db/schema.rb
@@ -59,8 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_17_075954) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.string "username"
     t.string "email"
     t.string "password_digest"
     t.boolean "admin"
