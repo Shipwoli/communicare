a55bee4ec448dd5fca0c4d865589591e2e9692f8
shipwoli <khalwalesaviour@gmail.com>
2023-04-28 15:38:12 +0300

    fixed area controller

--- Backend/app/controllers/areas_controller.rb
+++ Backend/app/controllers/areas_controller.rb
@@ -1,19 +1,53 @@
-class DonationsController < ApplicationController
-  before_action :authenticate_user!
+class AreasController < ApplicationController
+  before_action :set_area, only: [:show, :update, :destroy]
+  before_action :require_admin , only: [:create, :update, :destroy]
+
+  # def index
+  #   areas = Area.all
+  #   render json: areas
+  # end
+  def index
+    areas = Area.includes(:communicable_disease).all
+    render json: areas.to_json(include: { communicable_disease: { only: [:name, :image_url] } })
+
+  end
+  
+
+  def show
+    render json: @area
+  end
 
   def create
-    @donation = current_user.donations.build(donation_params)
+    area = Area.new(area_params)
 
-    if @donation.save
-      render json: { status: :success, donation: @donation }
+    if area.save
+      render json: area, status: :created
     else
-      render json: { status: :error, message: @donation.errors.full_messages.join(', ') }
+      render json: area.errors, status: :unprocessable_entity
     end
+  end
+
+  def update
+    if @area.update(area_params)
+      render json: @area
+    else
+      render json: @area.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @area.destroy
+    head :no_content
   end
 
   private
 
-  def donation_params
-    params.require(:donation).permit(:amount, :area_id)
+  def set_area
+    @area = Area.find(params[:id])
+  end
+
+  def area_params
+    params.require(:area).permit(:name, :location, :latitude, :longitude, :reported_cases, :communicable_disease_id)
+
   end
 end
